0b647e1a6ba0f54f7e9cc4c8f00a4fc504ca759b
nerokome <nerooghenekome@gmail.com>
2025-07-22 20:20:49 +0100

    hhhh

--- my-app/src/components/Dong.jsx
+++ my-app/src/components/Dong.jsx
@@ -3,17 +3,30 @@ import { motion } from 'framer-motion';
 
 const Dong = () => {
   return (
-    <div className="bg-black h-[300px]  justify-center">
+    <div className="bg-black   justify-center">
       <motion.div
-        className="flex flex-col md:flex-row  justify-center text-white gap-6"
+        className="flex flex-col md:flex-row   text-white gap-6 "
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.3, duration: 0.5 }}
       >
-        <ul className="space-y-2 md:space-y-0 md:space-x-6 flex flex-col md:flex-row  text-center text-2xl pt-10">
+              <motion.div
+                  className="flex items-center gap-2 self-start"
+                  initial={{ x: -50, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.6 }}
+                >
+                  <img
+                    src="/sca.png"
+                    alt="SCAH Logo"
+                    className="w-32 h-auto md:w-40 object-contain"
+                  />
+                </motion.div>
+                <div className='flex items-start md:items-center md:px-150 '>
+                   <ul className="space-y-2 md:space-y-0 md:space-x-6 flex flex-col md:flex-row p-5  text-start md:pt-3   md:text-xl md:text-center  text-lg ">
           <li>
             <a href="/home" className="hover:underline">
-              Homepage
+              Home
             </a>
           </li>
           <li>
@@ -26,12 +39,10 @@ const Dong = () => {
               Help
             </a>
           </li>
-          <li>
-            <a href="/contact" className="hover:underline">
-              Contacts
-            </a>
-          </li>
+  
         </ul>
+                </div>
+       
       </motion.div>
     </div>
   );
