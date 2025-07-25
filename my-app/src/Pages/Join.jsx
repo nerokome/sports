25c65353c6bfd6afb7437e092325266e58a3d3a3
nerokome <nerooghenekome@gmail.com>
2025-07-25 13:51:10 +0100

    grattay

--- my-app/src/Pages/Join.jsx
+++ my-app/src/Pages/Join.jsx
@@ -77,7 +77,7 @@ const Join = () => {
         <div className="md:w-1/2 w-full flex items-center justify-center px-6 py-12">
           <div className="w-full max-w-2xl text-center">
             <div className="flex flex-wrap justify-center items-center gap-2 sm:gap-4">
-              <p className="text-3xl sm:text-4xl font-semibold">Scout,</p>
+              <p className="text-3xl pt-10 sm:pt-0 sm:text-4xl font-semibold">Scout,</p>
               <p className="text-3xl sm:text-4xl font-semibold">
                 Spot and Develop Talent
               </p>
